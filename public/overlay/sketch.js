e8ecda1ae5c00fd1c578d6cc61fdae3aafbf1c69
danielmuthama <daniel.muthama@student.moringaschool.com>
2022-05-12 11:20:31 +0300

    update files

--- public/overlay/sketch.js
+++ public/overlay/sketch.js
@@ -1,11 +1,11 @@
-
 let myData;
 let img
-function preload(){
+
+function preload() {
     myData = loadJSON('/api');
 }
 
-function setup(){
+function setup() {
     // createCanvas(800, 800)
     noCanvas();
     noLoop();
@@ -14,21 +14,20 @@ function setup(){
 }
 
 
-function draw(){
+function draw() {
 
-    for(let p in myData){
+    for (let p in myData) {
         let item = myData[p];
-        
+
         img = createImg(item.image)
-        // img.hide()
+            // img.hide()
         img.size(800, 800)
         img.elt.style.position = 'fixed';
-        img.position(20,20)
+        img.position(20, 20)
         img.elt.style.opacity = "0.5";
 
-        // const l = ceil(width/Object.keys(myData).length)
-        // image(img, 0,0, 800, 800)
-        
+
+
     }
 
 }
